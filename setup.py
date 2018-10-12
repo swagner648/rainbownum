e3620abb63d0c8b938e4e5f52184fccfc1a437eb
simonwagner <swagner648@gmail.com>
2018-10-12 12:56:35 -0500

    Create setup file

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="rainbownums",
+    version="1.2",
+    author="Simon Wagner",
+    author_email="swagner648@gmail.com",
+    description="Python package to solve anti-Schur equations",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+)
